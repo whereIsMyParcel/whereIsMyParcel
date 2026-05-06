2bcb2ffaed8f68a692fb340cc8f2f3ef03385669
jaehoon0321 <jaehoon0321@gmail.com>
2026-05-06 15:35:08 +0900

    chore: 멀티 모듈 루트 프로젝트 초기 설정 (Java 17, Spring Boot 3.5.14)

---
+++ src/main/java/com/sparta/whereismyparcel/WhereismyparcelApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.whereismyparcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereismyparcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereismyparcelApplication.class, args);
+	}
+
+}
