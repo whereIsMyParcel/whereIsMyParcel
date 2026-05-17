98825976ba508c9f2b2de2deaf74921b636d6cf7
kimjaejoong <wcn07003@naver.com>
2026-05-17 10:15:56 +0900

    chore: 도메인 서비스 모듈 뼈대 추가

---
+++ services/ai-slack-service/src/main/java/com/sparta/whereismyparcel/notification/AiSlackServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sparta.whereismyparcel.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSlackServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiSlackServiceApplication.class, args);
+	}
+}
---
+++ services/hub-service/src/main/java/com/sparta/whereismyparcel/hub/HubServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sparta.whereismyparcel.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubServiceApplication.class, args);
+	}
+}
