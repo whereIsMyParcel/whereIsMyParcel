ef7f6d36be7914ed00b011486d97dc4ca1e25b7c
skdev0619 <skdev0619@naver.com>
2026-05-21 18:34:36 +0900

    feat: 배송담당자 service 추가

---
+++ services/shipment-service/src/main/java/com/sparta/whereismyparcel/shipment/application/service/DeliveryManagerService.java
@@ -0,0 +1,30 @@
+package com.sparta.whereismyparcel.shipment.application.service;
+
+import com.sparta.whereismyparcel.shipment.domain.entity.DeliveryManager;
+import com.sparta.whereismyparcel.shipment.domain.repository.DeliveryManagerRepository;
+import com.sparta.whereismyparcel.shipment.presentation.dto.request.DeliveryManagerCreateRequest;
+import com.sparta.whereismyparcel.shipment.presentation.dto.response.DeliveryManagerCreateResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DeliveryManagerService {
+
+    private final DeliveryManagerPolicy deliveryManagerPolicy;
+    private final DeliveryManagerRepository deliveryManagerRepository;
+
+    public DeliveryManagerCreateResponse create(DeliveryManagerCreateRequest request) {
+        //1. 생성 정책 확인
+        deliveryManagerPolicy.checkCreate(request.slackId(), request.type(), request.hubId());
+        //2. 배송담당자 순번 채번
+        int newOrder = deliveryManagerRepository.findNextDeliveryOrder(request.hubId(), request.type());
+        //3. 저장
+        DeliveryManager saved = deliveryManagerRepository.save(
+                DeliveryManager.create(request.hubId(), request.slackId(), request.type(), newOrder)
+        );
+
+        return DeliveryManagerCreateResponse.from(saved);
+    }
+
+}
