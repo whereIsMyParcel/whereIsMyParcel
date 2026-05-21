c7b897f11345f91a7f0e655e1dd963997e89f18d
skdev0619 <skdev0619@naver.com>
2026-05-21 18:33:32 +0900

    feat: repository 수정(담당자 수 카운트, 담당자 순서 채번)

--- services/shipment-service/src/main/java/com/sparta/whereismyparcel/shipment/domain/repository/DeliveryManagerRepository.java
+++ services/shipment-service/src/main/java/com/sparta/whereismyparcel/shipment/domain/repository/DeliveryManagerRepository.java
@@ -1,9 +1,22 @@
 package com.sparta.whereismyparcel.shipment.domain.repository;
 
 import com.sparta.whereismyparcel.shipment.domain.entity.DeliveryManager;
+import com.sparta.whereismyparcel.shipment.domain.entity.DeliveryType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface DeliveryManagerRepository extends JpaRepository<DeliveryManager, UUID> {
+    long countByHubIdAndType(UUID hubId, DeliveryType type);
+    //TODO 동시성 이슈 발생 가능성 높으므로 변경 필요
+    @Query("""
+        select coalesce(max(d.deliveryOrder), 0) + 1
+        from DeliveryManager d
+        where d.hubId = :hubId
+          and d.type = :type
+    """)
+    int findNextDeliveryOrder(@Param("hubId") UUID hubId,
+                              @Param("type") DeliveryType type);
 }
