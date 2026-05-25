8a3c49a4cca45a9a3930f1cd633d9fa502f0a041
skdev0619 <skdev0619@naver.com>
2026-05-25 22:12:18 +0900

    feat: 배송 취소 테스트 nested로 묶음

--- services/shipment-service/src/test/java/com/sparta/whereismyparcel/shipment/domain/entity/ShipmentTest.java
+++ services/shipment-service/src/test/java/com/sparta/whereismyparcel/shipment/domain/entity/ShipmentTest.java
@@ -2,6 +2,7 @@ package com.sparta.whereismyparcel.shipment.domain.entity;
 
 import com.sparta.whereismyparcel.shipment.domain.exception.ShipmentAlreadyStartedException;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
@@ -15,93 +16,98 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class ShipmentTest {
 
-    @DisplayName("배송이 시작되기 전 상태에서만 배송 취소가 가능하다")
-    @ParameterizedTest
-    @EnumSource(value = ShipmentStatus.class, names = "HUB_WAITING")
-    void canCancel(ShipmentStatus status) {
-        Shipment shipment = Shipment.create(
-                UUID.randomUUID(),
-                UUID.randomUUID(),
-                UUID.randomUUID(),
-                UUID.randomUUID(),
-                "배송번호 1",
-                status,
-                "배송지 주소",
-                "수령인명",
-                "slack id"
-        );
+    @DisplayName("배송 취소")
+    @Nested
+    class cancel {
 
-        shipment.cancel();
+        @DisplayName("배송이 시작되기 전 상태에서만 배송 취소가 가능하다")
+        @ParameterizedTest
+        @EnumSource(value = ShipmentStatus.class, names = "HUB_WAITING")
+        void canCancel(ShipmentStatus status) {
+            Shipment shipment = Shipment.create(
+                    UUID.randomUUID(),
+                    UUID.randomUUID(),
+                    UUID.randomUUID(),
+                    UUID.randomUUID(),
+                    "배송번호 1",
+                    status,
+                    "배송지 주소",
+                    "수령인명",
+                    "slack id"
+            );
 
-        assertThat(shipment.getShipmentStatus())
-                .isEqualTo(ShipmentStatus.CANCELLED);
-    }
+            shipment.cancel();
 
-    @DisplayName("배송이 시작된 상태에서는 배송 취소 시 예외가 발생한다")
-    @ParameterizedTest
-    @EnumSource(value = ShipmentStatus.class, mode = EnumSource.Mode.EXCLUDE, names = "HUB_WAITING")
-    void canNotCancel(ShipmentStatus status) {
-        Shipment shipment = Shipment.create(
-                UUID.randomUUID(),
-                UUID.randomUUID(),
-                UUID.randomUUID(),
-                UUID.randomUUID(),
-                "배송번호 1",
-                status,
-                "배송지 주소",
-                "수령인명",
-                "slack id"
-        );
+            assertThat(shipment.getShipmentStatus())
+                    .isEqualTo(ShipmentStatus.CANCELLED);
+        }
 
-        assertThatThrownBy(shipment::cancel)
-                .isInstanceOf(ShipmentAlreadyStartedException.class);
-    }
+        @DisplayName("배송이 시작된 상태에서는 배송 취소 시 예외가 발생한다")
+        @ParameterizedTest
+        @EnumSource(value = ShipmentStatus.class, mode = EnumSource.Mode.EXCLUDE, names = "HUB_WAITING")
+        void canNotCancel(ShipmentStatus status) {
+            Shipment shipment = Shipment.create(
+                    UUID.randomUUID(),
+                    UUID.randomUUID(),
+                    UUID.randomUUID(),
+                    UUID.randomUUID(),
+                    "배송번호 1",
+                    status,
+                    "배송지 주소",
+                    "수령인명",
+                    "slack id"
+            );
 
-    @DisplayName("해당 유저가 담당 배송 담당자인지 여부를 확인한다")
-    @ParameterizedTest
-    @MethodSource("managerProvider")
-    void isAssignedDeliveryManager(boolean expected, UUID assignedManagerId, UUID requestManagerId) {
-        // given
-        Shipment shipment = Shipment.create(
-                UUID.randomUUID(),
-                UUID.randomUUID(),
-                UUID.randomUUID(),
-                UUID.randomUUID(),
-                "배송번호 1",
-                ShipmentStatus.HUB_WAITING,
-                "배송지 주소",
-                "수령인명",
-                "slack id"
-        );
+            assertThatThrownBy(shipment::cancel)
+                    .isInstanceOf(ShipmentAlreadyStartedException.class);
+        }
 
-        ShipmentHistory history = ShipmentHistory.create(
-                shipment,
-                UUID.randomUUID(),
-                1,
-                UUID.randomUUID(),
-                assignedManagerId,
-                ShipmentStatus.HUB_WAITING,
-                0,
-                0,
-                0,
-                "test"
-        );
+        @DisplayName("해당 유저가 담당 배송 담당자인지 여부를 확인한다")
+        @ParameterizedTest
+        @MethodSource("managerProvider")
+        void isAssignedDeliveryManager(boolean expected, UUID assignedManagerId, UUID requestManagerId) {
+            // given
+            Shipment shipment = Shipment.create(
+                    UUID.randomUUID(),
+                    UUID.randomUUID(),
+                    UUID.randomUUID(),
+                    UUID.randomUUID(),
+                    "배송번호 1",
+                    ShipmentStatus.HUB_WAITING,
+                    "배송지 주소",
+                    "수령인명",
+                    "slack id"
+            );
 
-        shipment.getHistories().add(history);
+            ShipmentHistory history = ShipmentHistory.create(
+                    shipment,
+                    UUID.randomUUID(),
+                    1,
+                    UUID.randomUUID(),
+                    assignedManagerId,
+                    ShipmentStatus.HUB_WAITING,
+                    0,
+                    0,
+                    0,
+                    "test"
+            );
 
-        // when
-        boolean result = shipment.isAssignedDeliveryManager(requestManagerId);
+            shipment.getHistories().add(history);
 
-        // then
-        assertThat(result).isEqualTo(expected);
-    }
+            // when
+            boolean result = shipment.isAssignedDeliveryManager(requestManagerId);
 
-    static Stream<Arguments> managerProvider() {
-        UUID assignedManagerId = UUID.randomUUID();
+            // then
+            assertThat(result).isEqualTo(expected);
+        }
 
-        return Stream.of(
-                Arguments.of(true, assignedManagerId, assignedManagerId),
-                Arguments.of(false, assignedManagerId, UUID.randomUUID())
-        );
+        static Stream<Arguments> managerProvider() {
+            UUID assignedManagerId = UUID.randomUUID();
+
+            return Stream.of(
+                    Arguments.of(true, assignedManagerId, assignedManagerId),
+                    Arguments.of(false, assignedManagerId, UUID.randomUUID())
+            );
+        }
     }
 }
