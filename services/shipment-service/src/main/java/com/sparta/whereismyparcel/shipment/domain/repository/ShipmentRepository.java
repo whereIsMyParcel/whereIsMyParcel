939ba29c430aaf37092228067d4a1fc9d081a70e
skdev0619 <skdev0619@naver.com>
2026-05-25 21:18:47 +0900

    feat:주문에 속한 배송들 조회

--- services/shipment-service/src/main/java/com/sparta/whereismyparcel/shipment/domain/repository/ShipmentRepository.java
+++ services/shipment-service/src/main/java/com/sparta/whereismyparcel/shipment/domain/repository/ShipmentRepository.java
@@ -3,7 +3,9 @@ package com.sparta.whereismyparcel.shipment.domain.repository;
 import com.sparta.whereismyparcel.shipment.domain.entity.Shipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ShipmentRepository extends JpaRepository<Shipment, UUID> {
+    List<Shipment> findAllByOrderId(UUID orderId);
 }
