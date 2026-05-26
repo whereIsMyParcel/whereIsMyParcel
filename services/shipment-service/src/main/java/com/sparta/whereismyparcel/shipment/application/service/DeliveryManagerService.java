b91e2d16e1e3e7181cbb385336ff53d3f3ceca08
skdev0619 <skdev0619@naver.com>
2026-05-26 21:32:16 +0900

    feat: 배송 담당자 배정

--- services/shipment-service/src/main/java/com/sparta/whereismyparcel/shipment/application/service/DeliveryManagerService.java
+++ services/shipment-service/src/main/java/com/sparta/whereismyparcel/shipment/application/service/DeliveryManagerService.java
@@ -2,13 +2,16 @@ package com.sparta.whereismyparcel.shipment.application.service;
 
 import com.sparta.whereismyparcel.shipment.domain.entity.DeliveryManager;
 import com.sparta.whereismyparcel.shipment.domain.entity.DeliveryType;
+import com.sparta.whereismyparcel.shipment.domain.exception.NoAvailableDeliveryManagerException;
 import com.sparta.whereismyparcel.shipment.domain.repository.DeliveryManagerRepository;
 import com.sparta.whereismyparcel.shipment.presentation.dto.request.DeliveryManagerCreateRequest;
 import com.sparta.whereismyparcel.shipment.presentation.dto.response.DeliveryManagerCreateResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -43,4 +46,37 @@ public class DeliveryManagerService {
         };
     }
 
+    public List<UUID> assignHubDeliveryManagers(int count) {
+        return assign(null, DeliveryType.HUB_DELIVERY, count);
+    }
+
+    public List<UUID> assignCompanyDeliveryManagers(UUID hubId, int count) {
+        return assign(hubId, DeliveryType.COMPANY_DELIVERY, count);
+    }
+
+    private List<UUID> assign(UUID hubId, DeliveryType type, int count) {
+        List<DeliveryManager> deliveryManagers = switch (type) {
+            // 배송 타입에 따라 배정 대상 배송 담당자 조회
+            case HUB_DELIVERY ->
+                    deliveryManagerRepository.findNextHubDeliveryManagers(
+                            type,
+                            Pageable.ofSize(count)
+                    );
+
+            case COMPANY_DELIVERY ->
+                    deliveryManagerRepository.findNextCompanyDeliveryManagers(
+                            hubId,
+                            type,
+                            Pageable.ofSize(count)
+                    );
+        };
+        // 배정 가능한 배송 담당자가 없는 경우 예외 발생
+        if (deliveryManagers.isEmpty()) {
+            throw new NoAvailableDeliveryManagerException();
+        }
+        // 배송 담당자 ID 목록 반환
+        return deliveryManagers.stream()
+                .map(DeliveryManager::getId)
+                .toList();
+    }
 }
