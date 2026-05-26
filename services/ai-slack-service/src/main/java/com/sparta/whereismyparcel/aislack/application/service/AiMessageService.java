6dff3a719173f05ce3cad08e39863d501ebdcfd1
jimin Kim <gjimin26@gmail.com>
2026-05-26 14:45:51 +0900

    [feat] ArchitectureTest 적용

--- services/ai-slack-service/src/main/java/com/sparta/whereismyparcel/aislack/application/service/AiMessageService.java
+++ services/ai-slack-service/src/main/java/com/sparta/whereismyparcel/aislack/application/service/AiMessageService.java
@@ -1,4 +1,19 @@
 package com.sparta.whereismyparcel.aislack.application.service;
 
+import com.sparta.whereismyparcel.aislack.infrastructure.client.OrderFeignClient;
+import com.sparta.whereismyparcel.aislack.infrastructure.client.ShipmentFeignClient;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class AiMessageService {
+
+    private final OrderFeignClient orderFeignClient;
+    private final ShipmentFeignClient shipmentFeignClient;
+
+    @Transactional
 }
