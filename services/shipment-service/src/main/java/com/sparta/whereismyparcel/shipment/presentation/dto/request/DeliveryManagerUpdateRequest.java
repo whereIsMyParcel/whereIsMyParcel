fc2a349400f11ba0c43bf496a756d64afba44d09
skdev0619 <skdev0619@naver.com>
2026-05-27 01:30:24 +0900

    feat: 배송담당자, 배송 관련 request, response 추가

---
+++ services/shipment-service/src/main/java/com/sparta/whereismyparcel/shipment/presentation/dto/request/DeliveryManagerUpdateRequest.java
@@ -0,0 +1,11 @@
+package com.sparta.whereismyparcel.shipment.presentation.dto.request;
+
+import com.sparta.whereismyparcel.shipment.domain.entity.DeliveryType;
+
+import java.util.UUID;
+
+public record DeliveryManagerUpdateRequest(
+        UUID hubId,
+        String slackId,
+        DeliveryType type
+) {}
